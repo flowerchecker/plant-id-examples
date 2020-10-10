3d948f70ae2c5831095dc0a9a6fd293fb1ae5178
Rui Lebre <ruilebre@ua.pt>
2020-10-10 15:56:09 +0100

    Implementation of sync example for node using native libs

---
+++ node/sync_identification_example_https.js
@@ -0,0 +1,44 @@
+const https = require('https')
+var fs = require('fs');
+
+const files = ['photo1.jpg', 'photo2.jpg'];
+
+const base64files = files.map(file => fs.readFileSync(file, 'base64'));
+
+const data = JSON.stringify({
+    api_key: "-- ask for one: https://web.plant.id/api-access-request/ --",
+    images: base64files,
+    modifiers: ["crops_fast", "similar_images"],
+    plant_language: "en",
+    plant_details: ["common_names",
+        "url",
+        "name_authority",
+        "wiki_description",
+        "taxonomy",
+        "synonyms"]
+});
+
+const options = {
+    hostname: 'api.plant.id',
+    port: 443,
+    path: '/v2/identify',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': data.length
+    }
+}
+
+const req = https.request(options, res => {
+    res.on('data', d => {
+        process.stdout.write(d)
+    });
+});
+
+req.on('error', error => {
+    console.error('Error: ', error)
+});
+
+req.write(data)
+
+req.end()
