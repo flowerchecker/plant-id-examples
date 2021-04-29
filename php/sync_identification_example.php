d27ce9b71dd47f7760207f5cf39c6a864b2ca923
NateC <natechicago@users.noreply.github.com>
2021-04-29 08:57:11 -0500

    Reformatting PHP example

--- php/sync_identification_example.php
+++ php/sync_identification_example.php
@@ -1,36 +1,46 @@
 <?php
-function encodeImages($images){
-	$encoded_images = array();
-	foreach($images as $image){
-		$encoded_images[] = base64_encode(file_get_contents($image));
-	}
-	return $encoded_images;
+
+function encodeImages($images) {
+    $encoded_images = [];
+    foreach ($images as $image) {
+        $encoded_images[] = base64_encode(file_get_contents($image));
+    }
+    return $encoded_images;
 }
-function identifyPlants($file_names){
-	$encoded_images = encodeImages($file_names);
-	$api_key = "// ask for one: https://web.plant.id/api-access-request/";
-	$params = array(
-		"api_key" => $api_key,
-		"images" => $encoded_images,
-		"modifiers" => ["crops_fast", "similar_images"],
-		"plant_language" => "en",
-		"plant_details" => array("common_names",
-							"url",
-							"name_authority",
-							"wiki_description",
-							"taxonomy",
-							"synonyms"),
-		);
-	$params = json_encode($params);
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, "https://api.plant.id/v2/identify");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type:application/json"));
-	$result = curl_exec($ch);
-	curl_close($ch);
-	return $result;
+
+function identifyPlants($file_names) {
+    $api_key = 'ask for one: https://web.plant.id/api-access-request/';
+    $params  = [
+        'api_key'        => $api_key,
+        'images'         => encodeImages($file_names),
+        'modifiers'      => ['crops_fast', 'similar_images'],
+        'plant_language' => 'en',
+        'plant_details'  => [
+            'common_names',
+            'url',
+            'name_authority',
+            'wiki_description',
+            'taxonomy',
+            'synonyms'
+        ],
+    ];
+
+    $ch = curl_init();
+    curl_setopt_array(
+        $ch,
+        [
+            CURLOPT_URL            => 'https://api.plant.id/v2/identify',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_POST           => true,
+            CURLOPT_POSTFIELDS     => json_encode($params),
+            CURLOPT_HTTPHEADER     => ['Content-Type:application/json']
+        ]
+    );
+
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
 }
+
 print_r(identifyPlants(['photo1.jpg', 'photo2.jpg']));
-?>
