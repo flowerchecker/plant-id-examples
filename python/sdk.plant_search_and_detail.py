e8fcdf0b3f018f500e53dd24735f9b5e1aeebf31
Okeo <dan.kucera@flowerchecker.com>
2024-08-20 13:55:57 +0200

    feat: add sdk.plant_search_and_detail.py

--- python/sdk.plant_search_and_detail.py
+++ python/sdk.plant_search_and_detail.py
@@ -1,4 +1,4 @@
-import requests
+from kindwise import PlantApi, SearchResult, PlantKBType
 
 
 # Search plant knowledge base by query. Plants are searchable by scientific names, common names (in specified
@@ -6,46 +6,36 @@ import requests
 # by "Aloe", "Alo", "Vera", "ver", "Aloe vera"...
 
 
+api = PlantApi('your_api_key')
+
 plant_for_search = 'aloe vera'
+kb_type = PlantKBType.PLANTS    # str (optional, default plants), also 'diseases' can be searched.
 limit = 3        # int (optional) - maximum of returned result, max. 20, default 10
 language = 'de'  # two-letter ISO 639-1 language code (optional, default en) - language of common names can be
 # multiple (up to 3), separated by comma - i.e. (en,de,sv)
+search_result: SearchResult = api.search(plant_for_search, language=language, limit=limit, kb_type=kb_type)
 
-response = requests.get(
-    'https://plant.id/api/v3/kb/plants/name_search',
-    params={'q': plant_for_search, 'limit': limit, 'lang': language},
-    headers={'Api-Key': 'your_api_key'},
-)
-
-search = response.json()
-print(search)   # {'entities': [{'matched_in': 'Aloe vera', 'matched_in_type': 'entity_name', 'access_token':
-# 'RXE1Vxh5ZlYNOChOATB9TQ04CDh6Tgw0XWxQZ1FiVGU-', 'match_position': 0, 'match_length': 9, 'entity_name': 'Aloe
-# vera'}, {'matched_in': 'Aloe vera', 'matched_in_type': 'synonym', 'access_token':
-# 'E3FuV015NFYMOCpOCDB7TVo4Wjh6TgI0XGwBZ1BiXWU-', 'match_position': 0, 'match_length': 9, 'entity_name': 'Aloe
-# succotrina'}, {'matched_in': 'Aloe vera var. officinalis', 'matched_in_type': 'synonym', 'access_token':
-# 'EnExVxp5b1YAOHlOCDArTVw4CjgqTlE0XWxXZ1RiUmU-', 'match_position': 0, 'match_length': 9, 'entity_name': 'Aloe
-# officinalis'}], 'entities_trimmed': False, 'limit': 3}
+print(search_result)    # SearchResult(entities=[SearchEntity(matched_in='Aloe vera', matched_in_type='entity_name',
+# access_token='dkIkRiNCCDh7TixKf04CMgUwZlYDN3FJAzIAN3hLeEk-', match_position=0, match_length=9), SearchEntity(
+# matched_in='Aloe vera', matched_in_type='synonym', access_token='IEJ_RnZCWjh6Ti5Kdk4EMlIwNFYDN39JAjJRN3lLcUk-',
+# match_position=0, match_length=9), SearchEntity(matched_in='Aloe vera var. officinalis', matched_in_type='synonym',
+# access_token='IUIgRiFCATh2Tn1Kdk5UMlQwZFZTNyxJAzIHN31Lfkk-', match_position=0, match_length=9)],
+# entities_trimmed=False, limit=3)
 
 
 # Get entity details based on access token from Plant search endpoint.
 # One call cost 0.5 credits.
 
-
-access_token = search['entities'][0]['access_token']
+access_token = search_result.entities[0].access_token
 requested_plant_details = ('common_names,url,description,taxonomy,rank,gbif_id,inaturalist_id,image,synonyms,'
                            'edible_parts,watering,propagation_methods')
 # comma separated list of requested plant details (required) see detail description in documentation (
 # https://plant.id/docs)
 
-response = requests.get(
-    f'https://plant.id/api/v3/kb/plants/{access_token}',
-    params={'details': requested_plant_details, 'lang': language},
-    headers={'Api-Key': 'your_api_key'},
-)
-
-detail = response.json()
-print(detail)   # {'common_names': ['Echte Aloe'], 'taxonomy': {'class': 'Liliopsida', 'genus': 'Aloe', 'order':
-# 'Asparagales', 'family': 'Asphodelaceae', 'phylum': 'Tracheophyta', 'kingdom': 'Plantae'},
+entity_details = api.get_kb_detail(access_token, requested_plant_details, language=language, kb_type=kb_type)
+# entity_details = api.get_kb_detail(access_token, requested_plant_details, kb_type=kb_type)
+print(entity_details)   # {'common_names': ['Echte Aloe'], 'taxonomy': {'class': 'Liliopsida', 'genus': 'Aloe',
+# 'order': 'Asparagales', 'family': 'Asphodelaceae', 'phylum': 'Tracheophyta', 'kingdom': 'Plantae'},
 # 'url': 'https://de.wikipedia.org/wiki/Echte_Aloe', 'gbif_id': 2777724, 'inaturalist_id': 126882, 'rank': 'species',
 # 'description': {'value': 'Die Echte Aloe (Aloe vera) ist eine Pflanzenart aus der Gattung der Aloen (Aloe) in der
 # Unterfamilie der Affodillgewächse (Asphodeloideae). Das Artepitheton vera ist lateinisch und bedeutet u. a.
@@ -61,4 +51,3 @@ print(detail)   # {'common_names': ['Echte Aloe'], 'taxonomy': {'class': 'Liliop
 # 3.0', 'license_url': 'https://creativecommons.org/licenses/by/3.0/'}, 'edible_parts': ['seeds', 'leaves'],
 # 'watering': {'max': 1, 'min': 1}, 'propagation_methods': ['division', 'seeds'], 'language': 'de', 'entity_id':
 # '4ba05f1050481731', 'name': 'Aloe vera'}
-
