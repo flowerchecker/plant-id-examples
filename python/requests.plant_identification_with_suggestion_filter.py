948feefc8e8222971713950e45f01fc90a0f3036
Okeo <dan.kucera@flowerchecker.com>
2024-09-10 16:56:16 +0200

    feat: simplify suggestion_filter examples

--- python/requests.plant_identification_with_suggestion_filter.py
+++ python/requests.plant_identification_with_suggestion_filter.py
@@ -5,9 +5,10 @@ import requests
 with open('../images/unknown_plant.jpg', 'rb') as file:
     images = [base64.b64encode(file.read()).decode('ascii')]
 
-# suggestion_filter - dict (optional) in format {"classification": "FILTER_NAME (filter examples below)"}
-# Restricts the output of the model to specified list of classes (region or plant type) and adjusts the probabilities.
-# The lists can be combined with logical operators and parentheses. The lists are available https://plant.id/suggestion_filters.
+# suggestion_filter - dict (optional) in format {"classification": "FILTER_NAME (filter examples below)"} Restricts
+# the output of the model to specified list of classes (region or plant type) and adjusts the probabilities. The
+# lists can be combined with logical operators and parentheses. The lists are available here:
+# https://plant.id/suggestion_filters
 #
 # Examples:
 # Vegetables - {"classification": "vegetable"}
